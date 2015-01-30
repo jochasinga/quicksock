481797f74f52183682e3e5755cf6109e7b0f9bf0
Joe Chasinga <jo.chasinga@gmail.com>
2015-01-29 20:11:46 -0500

    add UDPClient

--- quicksock.go
+++ quicksock.go
@@ -2,21 +2,21 @@ package quicksock
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net"
 	"os"
-	"io/ioutil"
 )
 
-func Client(protocol string, host string, port string, request string) (net.Conn, error) {
+func Client(protocol, host string, port int, msg string) (net.Conn, error) {
 
 	fmt.Printf("Connecting to %s:%d", host, port)
-	if protocol == "tcp" {
-		addr := host + ":" + port
+
+	if protocol == "udp" {
+		UDPClient(host, port, msg)
 	}
-	else if protocol == "udp" {
-		// TODO: Handle UDP packet sending
-	}
-	
+
+	addr := host + ":" + string(port)
+
 	conn, err := net.Dial(protocol, addr)
 	checkError(err)
 
@@ -29,6 +29,28 @@ func Client(protocol string, host string, port string, request string) (net.Conn
 	fmt.Println(string(reply))
 
 	return conn, nil
+}
+
+func UDPClient(host string, port int, msg string) (*net.UDPConn, error) {
+
+	fmt.Printf("Sending packets to %s:%d", host, port)
+	addr, err := net.ResolveUDPAddr("udp", (host + ":" + int(port)))
+	checkError(err)
+
+	conn, err := net.DialUDP("udp", nil, addr)
+	checkError(err)
+
+	_, err = conn.Write([]byte(msg))
+	checkError(err)
+
+	result, err := ioutil.ReadAll(conn)
+	checkError(err)
+
+	fmt.Println(string(result))
+
+	return conn, nil
+
+}
 
 // Create a TCP Client that sends a HTTP request to a specified host
 func TCPClient(url string) (*net.TCPConn, error) {
@@ -37,13 +59,13 @@ func TCPClient(url string) (*net.TCPConn, error) {
 
 	addr, err := net.ResolveTCPAddr("tcp4", url)
 	checkError(err)
-	
+
 	conn, err := net.DialTCP("tcp", nil, addr)
 	checkError(err)
-	
+
 	_, err = conn.Write([]byte("HEAD / HTTP/1.0\r\n\r\n"))
 	checkError(err)
-	
+
 	result, err := ioutil.ReadAll(conn)
 	checkError(err)
 
@@ -56,7 +78,7 @@ func TCPServer(port string) (*net.TCPListener, error) {
 
 	addr, err := net.ResolveTCPAddr("tcp4", port)
 	checkError(err)
-	
+
 	listener, err := net.ListenTCP("tcp", addr)
 
 	for {
@@ -69,12 +91,10 @@ func TCPServer(port string) (*net.TCPListener, error) {
 		conn.Write([]byte(greeting))
 		defer conn.Close()
 	}
-
 	return listener, nil
-	
 }
 
-func checkError(err error)  {
+func checkError(err error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %s", err.Error())
 		os.Exit(1)
